aa2a67102a243beee83a0bb8ba39c3ffa1f6bccf
PC-NANAILLE\Rigal <anais.rigal29@hotmail.fr>
2017-11-21 10:57:50 +0100

    maj de creerEvenement.js

--- appclt/creerEvenement.js
+++ appclt/creerEvenement.js
@@ -1,14 +1,23 @@
 
 var app = angular.module('app', [])
-        .controller('Create', ['$scope',
-            function ($scope) {
+    app.controller('Create', function ($scope, $http) {
+        $scope.nomEvenmt = null;
+        $scope.description = null;
+        $scope.msg = null;
+        $scope.createEvent = function (nomEvenmt,description) {
+            var data = {
+                nomEvenmt: nomEvenmt,
+                description: description
+            };
+            $http.post('/createEvent', JSON.stringify(data)).then(function (response) {
+                if (response.data)
+                    $scope.msg = "Post Data Submitted Successfully!";
+            }, function (response) {
+                $scope.msg = "Service not Exists";
+            });
+            $scope.nomEvenmt='';
+            $scope.description='';
+        };
+    });
 
-
-                $scope.createEvent = function() {
-                    $scope.nomEvenmt="";
-                    $scope.description="";
-                    console.log("Hey");
-                }
-
-            }]);
 
